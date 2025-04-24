69e1db99901e8717e4ddd2b1f25e61ba79b826b4
essam03 <104560429+essam03@users.noreply.github.com>
2025-04-24 17:09:19 +0300

    commit 2

---
+++ University projcts/Summer 2023/website/New folder/home.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
+        integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Explora&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Explora&family=Pacifico&family=Satisfy&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>
+    <header id="header">
+        <div class="contain">
+            <div class="logo">
+            <a href="home.php"><img src="logo.png" alt="#" width="130"/></a>
+        </div>
+            <nav >
+                <ul>
+                    <li>
+                        <a href="home.php"> </i> Home</a>
+                    </li>
+                    <li>
+                        <a href="products.php"> Products</a>
+                    </li>
+                    <li>
+                        <a href="about.html"> </i> About us</a>
+                    </li>
+                    <li>
+                        <a href="contact.html"> </i> Contact</a>
+                    </li>
+                    <li>
+                        <a href="login.php"> </i> Account</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <h1 id="word"> JUICE BANGBANG <br> LET'S STAY FIT</h1>
+
+    <footer class="footer">
+        <h3> all copy rights are saved @ Sana Abu Zaid</h3>
+    </footer>
+</body>
+</html>
